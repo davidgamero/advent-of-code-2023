c77e1f47528ea3d6b4f28c2982586cb81018494b
David Gamero <david340804@gmail.com>
2023-12-08 12:32:52 -0500

    finally day 1 set me free

--- day1/day1p2.ts
+++ day1/day1p2.ts
@@ -44,11 +44,11 @@ class Trie {
 		for (let i = 0; i < word.length; i++) {
 			const char = word[i];
 			if (!c.children.has(char)) {
-				return ["", word.substring(0, i)];
+				return ["", word.substring(0, i+1)];
 			}
 			c = c.children.get(char)!;
 			if (c.value) {
-			return [c.value, word.substring(0, i)];
+			return [c.value, word.substring(0, i+1)];
 			}
 		}
 		return ["", ""];
@@ -60,42 +60,62 @@ for (const [word, num] of Object.entries(word2num)) {
 	trie.insert(word, num);
 }
 
-const firstAndLast2 = lines2.map((line) => {
-	let first = 0;
-	for (let i = 0; i < line.length; i++) {
-		const substr = line.substring(i);
-		// first check if it's a number
-		const char = substr[0];
+function getCalibrationSum(lines: string[]): number {
+	const values = lines.map((line) => {
+		let first = 0;
+		for (let i = 0; i < line.length; i++) {
+			const substr = line.substring(i);
+			// first check if it's a number
+			const char = substr[0];
 		if (!isNaN(parseInt(char))) {
 			first = parseInt(char);
 			break;
 		}
 		// then check if it's a word
 		const [value, word] = trie.search(substr);
-		if (value) {
-			first = parseInt(value);
-			break;
-		}
+	if (value) {
+		first = parseInt(value);
+		break;
 	}
-	let last = 0;
-	for (let i = line.length; i > 0; i--) {
-		const substr = line.substring(i);
-		// first check if it's a number
-		const char = substr[0];
+		}
+		let last = 0;
+		for (let i = line.length; i >= 0; i--) {
+			const substr = line.substring(i);
+			// first check if it's a number
+			const char = substr[0];
 		if (!isNaN(parseInt(char))) {
 			last = parseInt(char);
 			break;
 		}
 		// then check if it's a word
 		const [value, word] = trie.search(substr);
-		if (value) {
-			last = parseInt(value);
-			break;
-		}
+	if (value) {
+		last = parseInt(value);
+		break;
 	}
-	console.log('line is', line, 'first is', first, 'last is', last);
-	return (10*first)+last;
-});
+		}
+		let val = (10*first) + last;
+		console.log('line is', line, 'first is', first, 'last is', last, 'val is', val);
+		return val; 
+	});
+	console.log(values);
+	const sum = values.reduce((acc, curr) => acc + parseInt(curr), 0);
+	return sum;
+}
 
-const sum2 = firstAndLast2.reduce((acc, curr) => acc + parseInt(curr), 0);
-console.log(sum2);
+const testlines = [
+	"two1nine",
+	"eightwothree",
+	"abcone2threexyz",
+	"xtwone3four",
+	"4nineeightseven2",
+	"zoneight234",
+	"7pqrstsixteen",
+];
+const testsum = getCalibrationSum(testlines);
+
+const inputsum = getCalibrationSum(lines2);
+console.log('inputsum=', inputsum);
+
+//console.log(getCalibrationSum(["twone"]));
+
